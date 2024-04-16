c8267ac34336fdf880ccc1f355c76566a7fbad35
YouGoWoon <rhdnsd409@naver.com>
2024-04-16 16:19:30 +0900

    파일 생성

---
+++ src/main/java/com/example/beanricecakemall/BeanRiceCakeMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.beanricecakemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanRiceCakeMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanRiceCakeMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/beanricecakemall/BeanRiceCakeMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beanricecakemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanRiceCakeMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
