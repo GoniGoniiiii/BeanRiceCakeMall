bb7d8e8c948bf584d67edcd50f1b1999f0bfcebc
YouGoWoon <rhdnsd409@naver.com>
2024-04-17 02:01:00 +0900

    productList.html 추가, productList.css추가 , productDetail.html 페이지 생성

---
+++ src/main/java/com/example/beanricecakemall/MainController.java
@@ -0,0 +1,15 @@
+package com.example.beanricecakemall;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/productList")
+    public String productListP(){
+        return "/product/productList";
+
+    }
+
+}
