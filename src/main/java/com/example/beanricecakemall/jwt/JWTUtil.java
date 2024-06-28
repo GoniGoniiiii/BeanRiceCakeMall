a7ae267115360b9cb8e1b0e2168f487225ce674f
YouGoWoon <rhdnsd409@naver.com>
2024-06-28 14:24:16 +0900

    JWT검증을 위해 JWTUtil 클래스 추가

---
+++ src/main/java/com/example/beanricecakemall/jwt/JWTUtil.java
@@ -0,0 +1,13 @@
+package com.example.beanricecakemall.jwt;
+
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+@Component
+public class JWTUtil {
+
+    private SecretKey secretkey;
+
+
+}
