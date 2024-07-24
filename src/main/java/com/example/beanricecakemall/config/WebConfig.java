ac001e12e09a9ea30ed24d93fee380ee14a2f0eb
YouGoWoon <rhdnsd409@naver.com>
2024-07-25 01:20:13 +0900

    이미지업로드 위해 경로 추가

---
+++ src/main/java/com/example/beanricecakemall/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.beanricecakemall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    String resourcePath = "/upload/**";
+    String savePath = "D:\\goni\\image";
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(resourcePath)
+                .addResourceLocations("file:" +savePath+"/");
+    }
+}
