4f8b5c10046747f0af2dedd03718c6b541691652
YouGoWoon <rhdnsd409@naver.com>
2024-08-17 02:35:59 +0900

    수정

--- src/main/java/com/example/beanricecakemall/repository/DeliveryRepository.java
+++ src/main/java/com/example/beanricecakemall/repository/DeliveryRepository.java
@@ -4,4 +4,5 @@ import com.example.beanricecakemall.entity.DeliveryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DeliveryRepository extends JpaRepository<DeliveryEntity, Integer> {
+
 }
