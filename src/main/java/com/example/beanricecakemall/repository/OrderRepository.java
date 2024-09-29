352dfbb5f206a4802e3a8e5ebea30baf1fdb1d75
YouGoWoon <rhdnsd409@naver.com>
2024-09-30 01:55:53 +0900

    필요한 orderEntity 값을 뽑아쓰기 위해  추가

--- src/main/java/com/example/beanricecakemall/repository/OrderRepository.java
+++ src/main/java/com/example/beanricecakemall/repository/OrderRepository.java
@@ -3,8 +3,11 @@ package com.example.beanricecakemall.repository;
 import com.example.beanricecakemall.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<OrderEntity, Integer> {
     List<OrderEntity> findByUserEntityUserId(String userId);
+
+    OrderEntity findByOrderNum(int orderNum);
 }
