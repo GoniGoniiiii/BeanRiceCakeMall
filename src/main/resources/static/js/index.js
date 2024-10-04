aaf5d262dfcd7c6f528f74230818d6c60e02cf5a
YouGoWoon <rhdnsd409@naver.com>
2024-10-04 23:42:43 +0900

    숫자 1000단위마다 ,찍어주기

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -13,7 +13,21 @@ document.querySelectorAll('.sort-option').forEach(filter => {
             .then(response => response.text())
             .then(html => {
                 document.querySelector('#allProductList').innerHTML = html;
+                formatPrices();
             })
             .catch(error => console.error('Error : ', error));
     });
 });
+
+
+function formatPrices(){
+    const priceElements=document.querySelectorAll(".product-price");
+
+    priceElements.forEach(element =>{
+        const price=parseInt(element.textContent.replace(/[^0-9]/g,''));
+        if(!isNaN(price)){
+            element.textContent=price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")+'원';
+        }
+    });
+}
+window.onload=formatPrices;
