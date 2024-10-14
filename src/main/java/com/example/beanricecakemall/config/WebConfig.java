e45e9862ac946e3c3869e1a2cffdc978a58687fb
YouGoWoon <rhdnsd409@naver.com>
2024-10-14 23:57:11 +0900

    이미지 파일 업로드 경로 수정

--- src/main/java/com/example/beanricecakemall/config/WebConfig.java
+++ src/main/java/com/example/beanricecakemall/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     String resourcePath = "/upload/**";
-    String savePath = "C:\\goni\\image";
+    String savePath = "/home/ubuntu/bean/image/";
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
