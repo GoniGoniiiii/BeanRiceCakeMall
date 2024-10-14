1b6246be54c107cd8db3dea9b29f04f0ab3e54bc
YouGoWoon <rhdnsd409@naver.com>
2024-10-14 15:11:39 +0900

    product_num값으로 file_num 찾아오기

--- src/test/java/com/example/beanricecakemall/BeanRiceCakeMallApplicationTests.java
+++ src/test/java/com/example/beanricecakemall/BeanRiceCakeMallApplicationTests.java
@@ -1,13 +1,37 @@
 package com.example.beanricecakemall;
 
+import com.example.beanricecakemall.entity.FileEntity;
+import com.example.beanricecakemall.repository.FileRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class BeanRiceCakeMallApplicationTests {
+
+    @Autowired
+    private FileRepository fileRepository;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    public void testFindByProductEntityProductNum(){
+        int productNum=18;
+        List<FileEntity> fileEntity=fileRepository.findByProductEntityProductNum(productNum);
+        assertNotNull(fileEntity);
+
+        for(FileEntity fileEntity1:fileEntity){
+            System.out.println(fileEntity1.getFileNum());
+            System.out.println(fileEntity1.getFileUrl());
+            System.out.println(fileEntity1.getProductEntity().getProductNum());
+        }
+    }
 }
