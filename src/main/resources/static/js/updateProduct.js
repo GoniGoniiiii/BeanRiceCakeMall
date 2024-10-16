6072e4b0bd35f2e3f079a7172ede052df11a6f0b
YouGoWoon <rhdnsd409@naver.com>
2024-10-16 23:54:50 +0900

    상품 수정 페이지 js,css 분리 후 디테일수정

--- src/main/resources/static/js/updateProduct.js
+++ src/main/resources/static/js/updateProduct.js
@@ -4,3 +4,30 @@ function replaceEnter() {
     const formattedText = text.replace(/\n/g, '<br>');  // 줄바꿈 문자를 <br> 태그로 변환
     spanElement.innerHTML = formattedText;  // 변환된 HTML을 삽입
 }
+
+function imgDelete(button) {
+    event.preventDefault();
+    let file_url = button.value;
+    console.log(file_url);
+
+    fetch(`/img/delete/${encodeURIComponent(file_url)}`, {
+        method: 'POST',
+    })
+        .then(response => {
+            if (response.ok) {
+                console.log("파일 삭제 완료");
+                alert("이미지 삭제 완료");
+
+                // 이미지 박스를 DOM에서 제거
+                let imageBox = button.closest('.imagebox'); // 버튼의 가장 가까운 부모 .imagebox 찾기
+                if (imageBox) {
+                    imageBox.remove(); // 이미지 박스 삭제
+                }
+            } else {
+                console.log("파일 삭제 실패");
+            }
+        })
+        .catch(error => {
+            console.error("삭제 요청 중 오류 발생:", error);
+        });
+}
