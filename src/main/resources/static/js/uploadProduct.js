9837b8494f8627ed247575c6fef2cf355a69241d
YouGoWoon <rhdnsd409@naver.com>
2024-10-22 17:55:17 +0900

    이미지 추가시 미리보기 기능 추가

--- src/main/resources/static/js/uploadProduct.js
+++ src/main/resources/static/js/uploadProduct.js
@@ -1,35 +1,8 @@
-function replaceEnter() {
-    const spanElement = document.getElementById("product_content");
-    const text = spanElement.innerText || spanElement.textContent;  // 텍스트 콘텐츠를 가져옴
-    const formattedText = text.replace(/\n/g, '<br>');  // 줄바꿈 문자를 <br> 태그로 변환
-    spanElement.innerHTML = formattedText;  // 변환된 HTML을 삽입
-}
+function convertNewlineToBr() {
+    var content = document.getElementById("product_content").value; // 텍스트 에어리어에서 입력된 내용 가져오기
+    var convertedContent = content.replace(/\n/g, "<br>"); // \n을 <br>로 변환하기
 
-function imgDelete(button) {
-    event.preventDefault();
-    let file_url = button.value;
-    console.log(file_url);
-
-    fetch(`/img/delete/${encodeURIComponent(file_url)}`, {
-        method: 'POST',
-    })
-        .then(response => {
-            if (response.ok) {
-                console.log("파일 삭제 완료");
-                alert("이미지 삭제 완료");
-
-                // 이미지 박스를 DOM에서 제거
-                let imageBox = button.closest('.imagebox'); // 버튼의 가장 가까운 부모 .imagebox 찾기
-                if (imageBox) {
-                    imageBox.remove(); // 이미지 박스 삭제
-                }
-            } else {
-                console.log("파일 삭제 실패");
-            }
-        })
-        .catch(error => {
-            console.error("삭제 요청 중 오류 발생:", error);
-        });
+    document.getElementById("product_content").value = convertedContent; // 변환된 내용을 다시 텍스트 에어리어에 설정하기
 }
 
 const fileDOM = document.querySelector('.file');
